c492ad6bfde6ace088bc97127eb67d1d1986659b
MahmudJion <mahmudh.jion02@gmail.com>
2022-09-21 06:19:55 +0100

    review and update

--- Admin/addcategory.php
+++ Admin/addcategory.php
@@ -1,4 +1,6 @@
-<?php if(!isset($_SESSION)) { session_start(); } ?>
+<?php if (!isset($_SESSION)) {
+    session_start();
+}?>
 
 <!DOCTYPE html>
 <html>
@@ -13,10 +15,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-
-
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!--js--> 
+<!--js-->
 <script src="js/jquery.min.js"></script>
 
 <!--/js-->
@@ -25,55 +25,41 @@
 <!--header-->
 <!--sticky-->
 <?php
-if($_SESSION['loginstatus']=="")
-{
-	header("location:loginform.php");
+if ($_SESSION['loginstatus'] == "") {
+    header("location:loginform.php");
 }
 
 ?>
-<?php include('function.php'); ?>
+<?php include 'function.php';?>
 <?php
-if(isset($_POST["sbmt"]))
-{
-	$cn=makeconnection();
-	$s="insert into category(Cat_name) values('" . $_POST["t1"] ."')";
-	mysqli_query($cn,$s);
-	mysqli_close($cn);
-	echo "<script>alert('Record Save');</script>";
+if (isset($_POST["sbmt"])) {
+    $cn = makeconnection();
+    $s = "insert into category(Cat_name) values('" . $_POST["t1"] . "')";
+    mysqli_query($cn, $s);
+    mysqli_close($cn);
+    echo "<script>alert('Record Save');</script>";
 }
 ?>
 
-
-
-<?php include('top.php'); ?>
+<?php include 'top.php';?>
 <!--/sticky-->
 <div style="padding-top:100px; box-shadow:1px 1px 20px black; min-height:570px" class="container">
 <div class="col-sm-3" style="border-right:1px solid #999; min-height:450px;">
-<?php include('left.php'); ?>
+<?php include 'left.php';?>
 </div>
 <div class="col-sm-9">
 
-
-
-
-<form method="post">
-<table border="0" width="400px" height="200px" align="center" class="tableshadow">
-<tr><td colspan="2" class="toptd">Add Category</td></tr>
-<tr><td class="lefttxt">Category Name</td><td><input type="text" name="t1" required pattern="[a-zA-z _]{3,20}" title"Please Enter Only Characters between 3 to 10 for Category Name" /></td></tr>
-<tr><td>&nbsp;</td><td ><input type="submit" value="SAVE" name="sbmt" /></td></tr>
-
-
-
-
-</table>
-</form>
-
-
+    <form method="post">
+        <table border="0" width="400px" height="200px" align="center" class="tableshadow">
+            <tr><td colspan="2" class="toptd">Add Category</td></tr>
+            <tr><td class="lefttxt">Category Name</td><td><input type="text" name="t1" required pattern="[a-zA-z _]{3,20}" title"Please Enter Only Characters between 3 to 10 for Category Name" /></td></tr>
+            <tr><td>&nbsp;</td><td ><input type="submit" value="SAVE" name="sbmt" /></td></tr>
+        </table>
+    </form>
 
 </div>
 
-
 </div>
-<?php include('bottom.php'); ?>
+<?php include 'bottom.php';?>
 </body>
 </html>
