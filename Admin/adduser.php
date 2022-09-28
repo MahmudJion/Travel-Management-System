cbe34c7d201d75b345842bcaa430fa82b204d8d7
MahmudJion <mahmudh.jion02@gmail.com>
2022-09-28 06:30:31 +0100

    review and update

--- Admin/adduser.php
+++ Admin/adduser.php
@@ -1,4 +1,6 @@
-<?php if(!isset($_SESSION)) { session_start(); } ?>
+<?php if (!isset($_SESSION)) {
+    session_start();
+} ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,11 +13,8 @@
 <link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-
-
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!--js--> 
+<!--js-->
 <script src="js/jquery.min.js"></script>
 
 <!--/js-->
@@ -24,26 +23,22 @@
 <!--header-->
 <!--sticky-->
 <?php
-if($_SESSION['loginstatus']=="")
-{
-	header("location:loginform.php");
+if ($_SESSION['loginstatus']=="") {
+    header("location:loginform.php");
 }
 ?>
 
 <?php include('function.php'); ?>
 
 <?php
-if(isset($_POST["sbmt"]))
-{
-	$cn=makeconnection();
-	$s="insert into users values('" . $_POST["t1"] ."','" . $_POST["t2"] . "','" . $_POST["s1"] . "')";
-	mysqli_query($cn,$s);
-	mysqli_close($cn);
-	echo "<script>alert('Record Save');</script>";
+if (isset($_POST["sbmt"])) {
+    $cn=makeconnection();
+    $s="insert into users values('" . $_POST["t1"] ."','" . $_POST["t2"] . "','" . $_POST["s1"] . "')";
+    mysqli_query($cn, $s);
+    mysqli_close($cn);
+    echo "<script>alert('Record Save');</script>";
 }
 ?>
-
-
 
 <?php include('top.php'); ?>
 <!--/sticky-->
@@ -53,25 +48,16 @@ if(isset($_POST["sbmt"]))
 </div>
 <div class="col-sm-9">
 
-
-
-
-<form method="post">
-<table border="0" width="400px" height="300px" align="center" class="tableshadow">
-<tr><td colspan="2" class="toptd">Add User</td></tr>
-<tr><td class="lefttxt">User Name</td><td><input type="text" name="t1" required pattern="[a-zA-z1 _]{3,50}" title"Please Enter Only Characters and numbers between 3 to 50 for User name" /></td></tr>
-<tr><td class="lefttxt">Password</td><td><input type="password" name="t2" required pattern="[a-zA-z0-9]{1,10}" title"Please Enter Only Characters and numbers between 1 to 10 for Company name"/></td></tr>
-<tr><td class="lefttxt">Confirm Password</td><td><input type="password" name="t3" required pattern="[a-zA-z0-9]{1,10}" title"Please Enter Only Characters and numbers between 1 to 10 for Company name"/></td></tr>
-<tr><td class="lefttxt">Type of User</td><td><select name="s1" required><option value="">Select</option><option value="Admin">Admin</option><option value="General">General</option></select></td></tr>
-<tr><td>&nbsp;</td><td ><input type="submit" value="SAVE" name="sbmt" /></td></tr>
-
-
-
-
-</table>
-</form>
-
-
+    <form method="post">
+        <table border="0" width="400px" height="300px" align="center" class="tableshadow">
+            <tr><td colspan="2" class="toptd">Add User</td></tr>
+            <tr><td class="lefttxt">User Name</td><td><input type="text" name="t1" required pattern="[a-zA-z1 _]{3,50}" title"Please Enter Only Characters and numbers between 3 to 50 for User name" /></td></tr>
+            <tr><td class="lefttxt">Password</td><td><input type="password" name="t2" required pattern="[a-zA-z0-9]{1,10}" title"Please Enter Only Characters and numbers between 1 to 10 for Company name"/></td></tr>
+            <tr><td class="lefttxt">Confirm Password</td><td><input type="password" name="t3" required pattern="[a-zA-z0-9]{1,10}" title"Please Enter Only Characters and numbers between 1 to 10 for Company name"/></td></tr>
+            <tr><td class="lefttxt">Type of User</td><td><select name="s1" required><option value="">Select</option><option value="Admin">Admin</option><option value="General">General</option></select></td></tr>
+            <tr><td>&nbsp;</td><td ><input type="submit" value="SAVE" name="sbmt" /></td></tr>
+        </table>
+    </form>
 
 </div>
 
