1550377cbff0d8b00ea08dd4df895b324107de7c
MahmudJion <mahmudh.jion02@gmail.com>
2022-09-28 06:30:55 +0100

    review and update

--- Admin/deletecategory.php
+++ Admin/deletecategory.php
@@ -1,4 +1,6 @@
-<?php if(!isset($_SESSION)) { session_start(); } ?>
+<?php if (!isset($_SESSION)) {
+    session_start();
+} ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,11 +13,8 @@
 <link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-
-
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!--js--> 
+<!--js-->
 <script src="js/jquery.min.js"></script>
 
 <!--/js-->
@@ -24,24 +23,21 @@
 <!--header-->
 <!--sticky-->
 <?php
-if($_SESSION['loginstatus']=="")
-{
-	header("location:loginform.php");
+if ($_SESSION['loginstatus']=="") {
+    header("location:loginform.php");
 }
 ?>
 
 <?php include('function.php'); ?>
 <?php
-if(isset($_POST["sbmt"]))
-{
-	$cn=makeconnection();
-	$s="delete from category  where Cat_id='" . $_POST["t1"] . "'";
-	mysqli_query($cn,$s);
-	mysqli_close($cn);
-	echo "<script>alert('Record Delete');</script>";
+if (isset($_POST["sbmt"])) {
+    $cn=makeconnection();
+    $s="delete from category  where Cat_id='" . $_POST["t1"] . "'";
+    mysqli_query($cn, $s);
+    mysqli_close($cn);
+    echo "<script>alert('Record Delete');</script>";
 }
 ?>
-
 
 <?php include('top.php'); ?>
 <!--/sticky-->
@@ -51,10 +47,6 @@ if(isset($_POST["sbmt"]))
 </div>
 <div class="col-sm-9">
 
-
-
-
-
 <form method="post">
 <table border="0" width="400px" height="200px" align="center" class="tableshadow">
 <tr><td colspan="2" class="toptd">Delete Category</td></tr>
@@ -63,40 +55,32 @@ if(isset($_POST["sbmt"]))
 <?php
 $cn=makeconnection();
 $s="select * from category";
-$result=mysqli_query($cn,$s);
+$result=mysqli_query($cn, $s);
 $r=mysqli_num_rows($result);
 //echo $r;
 
-while($data=mysqli_fetch_array($result))
-{
-	
-		echo "<option value=$data[0]>$data[0]</option>";
-	
+while ($data=mysqli_fetch_array($result)) {
+    echo "<option value=$data[0]>$data[0]</option>";
 }
 mysqli_close($cn);
-
-
 
 ?>
 
 </select>
 
 <?php
-if(isset($_POST["show"]))
-{
-$cn=makeconnection();
-$s="select * from category where Cat_id='" .$_POST["t1"] ."'";
-$result=mysqli_query($cn,$s);
-$r=mysqli_num_rows($result);
-//echo $r;
+if (isset($_POST["show"])) {
+    $cn=makeconnection();
+    $s="select * from category where Cat_id='" .$_POST["t1"] ."'";
+    $result=mysqli_query($cn, $s);
+    $r=mysqli_num_rows($result);
+    //echo $r;
 
-$data=mysqli_fetch_array($result);
-$Cat_id=$data[0];
-$Cat_name=$data[1];
+    $data=mysqli_fetch_array($result);
+    $Cat_id=$data[0];
+    $Cat_name=$data[1];
 
-
-mysqli_close($cn);
-
+    mysqli_close($cn);
 }
 
 ?>
