d8c52edfdc51e1f7fd5cc4cecc0a90b1487e154f
MahmudJion <mahmudh.jion02@gmail.com>
2022-09-29 06:37:07 +0100

    review and update

--- Admin/left.php
+++ Admin/left.php
@@ -1,4 +1,6 @@
-<?php if(!isset($_SESSION)) { session_start(); } ?>
+<?php if (!isset($_SESSION)) {
+    session_start();
+} ?>
 
 <!DOCTYPE html >
 <html>
@@ -11,8 +13,7 @@
 <table style="width:100%">
 <tr><td style="font-size:28px">Admin Links</td></tr>
 
-<?php if($_SESSION["usertype"]=="Admin")
-{?>
+<?php if ($_SESSION["usertype"]=="Admin") {?>
 
 <tr><td><a href="adduser.php">Add User</a></td></tr>
 <tr><td><a href="updateuser.php">Update User</a></td></tr>
@@ -22,18 +23,14 @@
 
 <tr><td><a href="addcategory.php">Add Category</a></td></tr>
 
-
-<?php if($_SESSION["usertype"]=="Admin")
-{?>
+<?php if ($_SESSION["usertype"]=="Admin") {?>
 <tr><td><a href="updatecategory.php">Update Category</a></td></tr>
 <tr><td><a href="deletecategory.php">Delete Category</a></td></tr>
 <?php }?>
 
-
 <tr><td><a href="viewcategory.php">View Category</a></td></tr>
 <tr><td><a href="addsubcategory.php">Add Subcategory</a></td></tr>
-<?php if($_SESSION["usertype"]=="Admin")
-{?>
+<?php if ($_SESSION["usertype"]=="Admin") {?>
 <tr><td><a href="updatesubcategory.php">Update Subcategory</a></td></tr>
 <tr><td><a href="deletesubcategory.php">Delete Subcategory</a></td></tr>
 <?php }?>
@@ -41,8 +38,7 @@
 <tr><td><a href="viewsubcategory.php">View Subcategory</a></td></tr>
 <tr><td><a href="addpackage.php">Add Package</a></td></tr>
 
-<?php if($_SESSION["usertype"]=="Admin")
-{?>
+<?php if ($_SESSION["usertype"]=="Admin") {?>
 <tr><td><a href="updatepackage.php">Update Package</a></td></tr>
 <tr><td><a href="deletepackage.php">Delete Package</a></td></tr>
 
