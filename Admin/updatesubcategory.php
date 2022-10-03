c02fb918d0b69b62ee9ccf02d30f0de281235fc1
MahmudJion <mahmudh.jion02@gmail.com>
2022-10-03 06:25:18 +0100

    review and update

--- Admin/updatesubcategory.php
+++ Admin/updatesubcategory.php
@@ -1,4 +1,6 @@
-<?php if(!isset($_SESSION)) { session_start(); } ?>
+<?php if (!isset($_SESSION)) {
+    session_start();
+} ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,11 +13,8 @@
 <link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-
-
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!--js--> 
+<!--js-->
 <script src="js/jquery.min.js"></script>
 
 <!--/js-->
@@ -24,52 +23,43 @@
 <!--header-->
 <!--sticky-->
 <?php
-if($_SESSION['loginstatus']=="")
-{
-	header("location:loginform.php");
+if ($_SESSION['loginstatus']=="") {
+    header("location:loginform.php");
 }
 ?>
 
 <?php include('function.php'); ?>
 
 <?php
-if(isset($_POST["sbmt"]))
-{
-	
-	$cn=makeconnection();
-	
-	$target_dir = "subcatimages/";
-	$target_file = $target_dir.basename($_FILES["t3"]["name"]);
-	$uploadok = 1;
-	$imagefiletype = pathinfo($target_file, PATHINFO_EXTENSION);
-	//check if image file is a actual image or fake image
-	
-	if(move_uploaded_file($_FILES["t3"]["tmp_name"], $target_file)){
-			
-	}else{
-			echo "sorry there was an error uploading your file.";
-		}
-		}
+if (isset($_POST["sbmt"])) {
+    $cn=makeconnection();
+
+    $target_dir = "subcatimages/";
+    $target_file = $target_dir.basename($_FILES["t3"]["name"]);
+    $uploadok = 1;
+    $imagefiletype = pathinfo($target_file, PATHINFO_EXTENSION);
+    //check if image file is a actual image or fake image
+
+    if (move_uploaded_file($_FILES["t3"]["tmp_name"], $target_file)) {
+    } else {
+        echo "sorry there was an error uploading your file.";
+    }
+}
 ?>
 
 <?php
-if(isset($_POST["sbmt"]))
-{
-	$cn=makeconnection();
+if (isset($_POST["sbmt"])) {
+    $cn=makeconnection();
 
-if (empty($_FILES['t3']['name'])) {
-	
-	$s="update subcategory set Subcatname='" . $_POST["t1"] ."',Catid='" . $_POST["t2"] . "',Pic='" . $_POST["h1"] . "',Detail='" . $_POST["t4"] . "' where Subcatid='" . $_POST["s1"] . "'";
-	
-}
-else
-{
-	
-		 $s="update subcategory set Subcatname='" . $_POST["t1"] ."',Catid='" . $_POST["t2"] . "',Pic='" .  basename($_FILES["t3"]["name"]) . "',Detail='" . $_POST["t4"] . "' where Subcatid='" . $_POST["s1"] . "'";}
-	mysqli_query($cn,$s);
-	mysqli_close($cn);
-	echo "<script>alert('Record Update');</script>";
+    if (empty($_FILES['t3']['name'])) {
+        $s="update subcategory set Subcatname='" . $_POST["t1"] ."',Catid='" . $_POST["t2"] . "',Pic='" . $_POST["h1"] . "',Detail='" . $_POST["t4"] . "' where Subcatid='" . $_POST["s1"] . "'";
+    } else {
+        $s="update subcategory set Subcatname='" . $_POST["t1"] ."',Catid='" . $_POST["t2"] . "',Pic='" .  basename($_FILES["t3"]["name"]) . "',Detail='" . $_POST["t4"] . "' where Subcatid='" . $_POST["s1"] . "'";
     }
+    mysqli_query($cn, $s);
+    mysqli_close($cn);
+    echo "<script>alert('Record Update');</script>";
+}
 ?>
 
 <?php include('top.php'); ?>
@@ -88,85 +78,80 @@ else
 <?php
 $cn=makeconnection();
 $s="select * from subcategory";
-$result=mysqli_query($cn,$s);
+$result=mysqli_query($cn, $s);
 $r=mysqli_num_rows($result);
 //echo $r;
 
-while($data=mysqli_fetch_array($result))
-{
-	if(isset($_POST["show"])&& $data[0]==$_POST["s1"])
-	{
-		echo"<option value=$data[0] selected>$data[1]</option>";
-	}
-	else
-	{
-		echo "<option value=$data[0]>$data[1]</option>";
-	}
+while ($data=mysqli_fetch_array($result)) {
+    if (isset($_POST["show"])&& $data[0]==$_POST["s1"]) {
+        echo"<option value=$data[0] selected>$data[1]</option>";
+    } else {
+        echo "<option value=$data[0]>$data[1]</option>";
+    }
 }
 mysqli_close($cn);
-
-
 
 ?>
 
 </select>
 <input type="submit" value="Show" name="show" formnovalidate/>
 <?php
-if(isset($_POST["show"]))
-{
-$cn=makeconnection();
-$s="select * from subcategory where subcatid='" .$_POST["s1"] ."'";
-$result=mysqli_query($cn,$s);
-$r=mysqli_num_rows($result);
-//echo $r;
+if (isset($_POST["show"])) {
+    $cn=makeconnection();
+    $s="select * from subcategory where subcatid='" .$_POST["s1"] ."'";
+    $result=mysqli_query($cn, $s);
+    $r=mysqli_num_rows($result);
+    //echo $r;
 
-$data=mysqli_fetch_array($result);
-$Subcatid=$data[0];
-$Subcatname=$data[1];
-$Catid=$data[2];
-$Pic=$data[3];
-$Detail=$data[4];
+    $data=mysqli_fetch_array($result);
+    $Subcatid=$data[0];
+    $Subcatname=$data[1];
+    $Catid=$data[2];
+    $Pic=$data[3];
+    $Detail=$data[4];
 
-mysqli_close($cn);
-
+    mysqli_close($cn);
 }
 
 ?>
 
 </td></tr>
 
-<tr><td class="lefttxt">Subcategory Name</td><td><input type="text" value="<?php if(isset($_POST["show"])){ echo $Subcatname ;} ?> " name="t1" required pattern="[a-zA-z1 _]{1,50}" title="Please Enter Only Characters and numbers between 1 to 50 for Subcategory Name" /></td></tr>
-<tr><td class="lefttxt">Select Category</td><td><select name="t2"  value="<?php if(isset($_POST["show"])){ echo $Catid ;} ?> " required="required" pattern="[a-zA-z1 _]{1,50}" title"Please Enter Only Characters and numbers between 1 to 50 for Company name"/><option value="Select">Select</option>
+<tr><td class="lefttxt">Subcategory Name</td><td><input type="text" value="<?php if (isset($_POST["show"])) {
+    echo $Subcatname ;
+} ?> " name="t1" required pattern="[a-zA-z1 _]{1,50}" title="Please Enter Only Characters and numbers between 1 to 50 for Subcategory Name" /></td></tr>
+<tr><td class="lefttxt">Select Category</td><td><select name="t2"  value="<?php if (isset($_POST["show"])) {
+    echo $Catid ;
+} ?> " required="required" pattern="[a-zA-z1 _]{1,50}" title"Please Enter Only Characters and numbers between 1 to 50 for Company name"/><option value="Select">Select</option>
 
 <?php
 $cn=makeconnection();
 $s="select * from category";
-$result=mysqli_query($cn,$s);
+$result=mysqli_query($cn, $s);
 $r=mysqli_num_rows($result);
 //echo $r;
 
-while($data=mysqli_fetch_array($result))
-{
-	if(isset($_POST["show"]) && $data[0]==$Catid)
-	{
-		echo "<option value=$data[0] selected>$data[1]</option>";
-	}
-	else
-	{
-		echo "<option value=$data[0]>$data[1]</option>";
-	}
-	
+while ($data=mysqli_fetch_array($result)) {
+    if (isset($_POST["show"]) && $data[0]==$Catid) {
+        echo "<option value=$data[0] selected>$data[1]</option>";
+    } else {
+        echo "<option value=$data[0]>$data[1]</option>";
+    }
 }
 mysqli_close($cn);
 ?>
 </select>
 
 <tr><td class="lefttxt">Old Pic</td><td><img src="subcatimages/<?php echo @$Pic; ?>" width="150px" height="100px" / >
-<input type="hidden" name="h1" value="<?php if(isset($_POST["show"])) {echo $Pic;} ?>" />
+<input type="hidden" name="h1" value="<?php if (isset($_POST["show"])) {
+    echo $Pic;
+} ?>" />
 </td></tr>
 
 <tr><td class="lefttxt">Upload Pic</td><td><input type="file" name="t3" /></td></tr>
-<tr><td class="lefttxt">Details</td><td><textarea name="t4" /><?php if(isset($_POST["show"])){ echo $Detail ;} ?></textarea></td></tr>
+<tr><td class="lefttxt">Details</td><td><textarea name="t4" /><?php if (isset($_POST["show"])) {
+    echo $Detail ;
+} ?></textarea></td></tr>
 <tr><td>&nbsp;</td><td ><input type="submit" value="Update" name="sbmt" /></td></tr>
 
 </table>
@@ -174,12 +159,8 @@ mysqli_close($cn);
 
 </div>
 
-
 </div>
 <?php include('bottom.php'); ?>
 
 </body>
 </html>
-
-
-             
