8f5804cab95780413762b94e2434b664f6582f9b
MahmudJion <mahmudh.jion02@gmail.com>
2022-10-03 06:25:50 +0100

    review and update

--- Admin/updateuser.php
+++ Admin/updateuser.php
@@ -1,4 +1,6 @@
-<?php if(!isset($_SESSION)) { session_start(); } ?>
+<?php if (!isset($_SESSION)) {
+    session_start();
+} ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,11 +13,8 @@
 <link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-
-
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!--js--> 
+<!--js-->
 <script src="js/jquery.min.js"></script>
 
 <!--/js-->
@@ -24,24 +23,21 @@
 <!--header-->
 <!--sticky-->
 <?php
-if($_SESSION['loginstatus']=="")
-{
-	header("location:loginform.php");
+if ($_SESSION['loginstatus']=="") {
+    header("location:loginform.php");
 }
 ?>
 
 <?php include('function.php'); ?>
 <?php
-if(isset($_POST["sbmt"]))
-{
-	$cn=makeconnection();
-	$s="update users set pwd='" . $_POST["t2"] ."',Typeofuser='" . $_POST["s1"] . "' where Username='" . $_POST["t1"] . "'";
-	mysqli_query($cn,$s);
-	mysqli_close($cn);
-	echo "<script>alert('Record Update');</script>";
+if (isset($_POST["sbmt"])) {
+    $cn=makeconnection();
+    $s="update users set pwd='" . $_POST["t2"] ."',Typeofuser='" . $_POST["s1"] . "' where Username='" . $_POST["t1"] . "'";
+    mysqli_query($cn, $s);
+    mysqli_close($cn);
+    echo "<script>alert('Record Update');</script>";
 }
 ?>
-
 
 <?php include('top.php'); ?>
 <!--/sticky-->
@@ -51,10 +47,6 @@ if(isset($_POST["sbmt"]))
 </div>
 <div class="col-sm-9">
 
-
-
-
-
 <form method="post">
 <table border="0" width="400px" height="300px" align="center" class="tableshadow">
 <tr><td colspan="2" class="toptd">Update User</td></tr>
@@ -63,61 +55,60 @@ if(isset($_POST["sbmt"]))
 <?php
 $cn=makeconnection();
 $s="select * from users";
-$result=mysqli_query($cn,$s);
+$result=mysqli_query($cn, $s);
 $r=mysqli_num_rows($result);
 //echo $r;
 
-while($data=mysqli_fetch_array($result))
-{
-	if(isset($_POST["show"])&& $data[0]==$_POST["t1"])
-	{
-		echo"<option value=$data[0] selected>$data[0]</option>";
-	}
-	else
-	{
-		echo "<option value=$data[0]>$data[0]</option>";
-	}
+while ($data=mysqli_fetch_array($result)) {
+    if (isset($_POST["show"])&& $data[0]==$_POST["t1"]) {
+        echo"<option value=$data[0] selected>$data[0]</option>";
+    } else {
+        echo "<option value=$data[0]>$data[0]</option>";
+    }
 }
 mysqli_close($cn);
-
-
 
 ?>
 
 </select>
 <input type="submit" value="Show" name="show" formnovalidate/>
 <?php
-if(isset($_POST["show"]))
-{
-$cn=makeconnection();
-$s="select * from users where Username='" .$_POST["t1"] ."'";
-$result=mysqli_query($cn,$s);
-$r=mysqli_num_rows($result);
-//echo $r;
+if (isset($_POST["show"])) {
+    $cn=makeconnection();
+    $s="select * from users where Username='" .$_POST["t1"] ."'";
+    $result=mysqli_query($cn, $s);
+    $r=mysqli_num_rows($result);
+    //echo $r;
 
-$data=mysqli_fetch_array($result);
-$Username=$data[0];
-$Pass=$data[1];
-$Usertype=$data[2];
+    $data=mysqli_fetch_array($result);
+    $Username=$data[0];
+    $Pass=$data[1];
+    $Usertype=$data[2];
 
-mysqli_close($cn);
-
+    mysqli_close($cn);
 }
 
 ?>
 
 </td></tr>
-<tr><td class="lefttxt">Password</td><td><input type="password"  value="<?php if(isset($_POST["show"])){ echo $Pass ;} ?>" name="t2" required pattern="[a-zA-z0-9]{1,10}" title"Please Enter Only Characters and numbers between 1 to 10 for Password"/></td></tr>
-<tr><td class="lefttxt">Confirm Password</td><td><input type="password" value="<?php if(isset($_POST["show"])){ echo $Pass ;} ?>" name="t3" required pattern="[a-zA-z0-9]{1,10}" title"Please Enter Only Characters and numbers between 1 to 10 for Password"/></td></tr>
+<tr><td class="lefttxt">Password</td><td><input type="password"  value="<?php if (isset($_POST["show"])) {
+    echo $Pass ;
+} ?>" name="t2" required pattern="[a-zA-z0-9]{1,10}" title"Please Enter Only Characters and numbers between 1 to 10 for Password"/></td></tr>
+<tr><td class="lefttxt">Confirm Password</td><td><input type="password" value="<?php if (isset($_POST["show"])) {
+    echo $Pass ;
+} ?>" name="t3" required pattern="[a-zA-z0-9]{1,10}" title"Please Enter Only Characters and numbers between 1 to 10 for Password"/></td></tr>
 <tr><td class="lefttxt">Type of User</td><td><select name="s1" required /><option value="">Select</option>
-<option value="Admin" <?php if(isset($_POST["show"])&& $Usertype=="Admin"){ echo "selected"; } ?>>Admin</option>
-<option value="General" <?php if(isset($_POST["show"])&& $Usertype=="General"){ echo "selected"; } ?>>General</option>
+<option value="Admin" <?php if (isset($_POST["show"])&& $Usertype=="Admin") {
+    echo "selected";
+} ?>>Admin</option>
+<option value="General" <?php if (isset($_POST["show"])&& $Usertype=="General") {
+    echo "selected";
+} ?>>General</option>
 </select></td></tr>
 <tr><td>&nbsp;</td><td ><input type="submit" value="Update" name="sbmt" /></td></tr>
 </table>
 </form>
 </div>
-
 
 </div>
 <?php include('bottom.php'); ?>
