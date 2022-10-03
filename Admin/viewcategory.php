bd3cd99f52c3e3ea30a2542f02fcef9e68e45fae
MahmudJion <mahmudh.jion02@gmail.com>
2022-10-03 06:26:28 +0100

    review and update

--- Admin/viewcategory.php
+++ Admin/viewcategory.php
@@ -1,4 +1,6 @@
-<?php if(!isset($_SESSION)) { session_start(); } ?>
+<?php if (!isset($_SESSION)) {
+    session_start();
+} ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,11 +13,8 @@
 <link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-
-
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!--js--> 
+<!--js-->
 <script src="js/jquery.min.js"></script>
 
 <!--/js-->
@@ -24,26 +23,21 @@
 <!--header-->
 <!--sticky-->
 <?php
-if($_SESSION['loginstatus']=="")
-{
-	header("location:loginform.php");
+if ($_SESSION['loginstatus']=="") {
+    header("location:loginform.php");
 }
 ?>
-
 
 <?php include('function.php'); ?>
 <?php
-if(isset($_POST["sbmt"]))
-{
-	$cn=makeconnection();
-	$s="insert into category(Cat_name) values('" . $_POST["t1"] ."')";
-	mysqli_query($cn,$s);
-	
-	echo "<script>alert('Record Save');</script>";
+if (isset($_POST["sbmt"])) {
+    $cn=makeconnection();
+    $s="insert into category(Cat_name) values('" . $_POST["t1"] ."')";
+    mysqli_query($cn, $s);
+
+    echo "<script>alert('Record Save');</script>";
 }
 ?>
-
-
 
 <?php include('top.php'); ?>
 <!--/sticky-->
@@ -52,9 +46,6 @@ if(isset($_POST["sbmt"]))
 <?php include('left.php'); ?>
 </div>
 <div class="col-sm-9">
-
-
-
 
 <form method="post">
 <table border="0" width="400px" height="300px" align="center" class="tableshadow">
@@ -67,19 +58,13 @@ if(isset($_POST["sbmt"]))
 <?php
 
 $s="select * from category";
-$result=mysqli_query($cn,$s);
+$result=mysqli_query($cn, $s);
 $r=mysqli_num_rows($result);
 //echo $r;
 
-while($data=mysqli_fetch_array($result))
-{
-	
-		echo "<tr><td style=' padding:5px;'>$data[0]</td><td style=' padding:5px;'>$data[1]</td></tr>";
-
+while ($data=mysqli_fetch_array($result)) {
+    echo "<tr><td style=' padding:5px;'>$data[0]</td><td style=' padding:5px;'>$data[1]</td></tr>";
 }
-
-
-
 
 ?>
 
@@ -88,10 +73,7 @@ while($data=mysqli_fetch_array($result))
 
 </form>
 
-
-
 </div>
-
 
 </div>
 <?php include('bottom.php'); ?>
