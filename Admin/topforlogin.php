4c30e3affadef563ab5b06eb6446449642a398e0
MahmudJion <mahmudh.jion02@gmail.com>
2022-10-03 06:23:48 +0100

    review and update

--- Admin/topforlogin.php
+++ Admin/topforlogin.php
@@ -1,15 +1,12 @@
-	 <div class="header-top">
-		 <!--container-->
-		  <div class="container">
-			 <div class="top-nav">
-						<div class="logo">
-						<a href="#"><img src="../images/" id="section-1" class="img-responsive" alt=""/></a>
-						</div>
-						
-			 </div>
-			  <div class="clearfix"> </div>
-			
-
-		 </div>
-		 <!--/container-->
-	 </div>
+<div class="header-top">
+    <!--container-->
+    <div class="container">
+        <div class="top-nav">
+                    <div class="logo">
+                    <a href="#"><img src="../images/" id="section-1" class="img-responsive" alt=""/></a>
+                    </div>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+    <!--/container-->
+</div>
