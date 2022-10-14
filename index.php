a2e63d5d832b69abcc92fd476c68cf5151486969
MahmudJion <mahmudh.jion02@gmail.com>
2022-10-14 11:54:44 +0100

    review and update

--- index.php
+++ index.php
@@ -6,11 +6,9 @@
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
- //function hideURLbar(){ window.scrollTo(0,1); } 
- 
- 
- </script>
-<!--js--> 
+ //function hideURLbar(){ window.scrollTo(0,1); }
+</script>
+<!--js-->
 <script src="js/jquery.min.js"></script>
 
 </head>
@@ -27,7 +25,7 @@
 	  <div class="about-head text-center">
 	  <h3>About Us</h3>
 	  </div>
-	   <div class="container">		  
+	   <div class="container">
 		 <div class="col-md-4 about-grids">
 			 <h4>Our Vision</h4>
 			 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -43,11 +41,11 @@
 	 </div>
 </div>
 <!--/About-->
-<!--top-tours-->	
+<!--top-tours-->
 <div  class="section" id="section-3">
 <div id="portfolio" class="portfolio">
    <div class="top-tours-head text-center">
-		  <h3>Gallery</h3>		 
+		  <h3>Gallery</h3>
 		  </div>
 	      <ul id="filters" class="clearfix wow bounceIn" data-wow-delay="0.4s">
 			<li><span class="filter active" data-filter="app card icon logo fun">ALL</span></li>
@@ -58,66 +56,65 @@
 	        </ul>
 	     <div id="portfoliolist">
 					<div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
-						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
 							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
 						     <img src="images/t1.jpg" class="img-responsive" alt=""/></a>
 							</div>
-					</div>				
+					</div>
 					<div class="portfolio app mix_all" data-cat="app" style="display: inline-block; opacity: 1;">
-						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
 							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
 						     <img src="images/t2.jpg" class="img-responsive" alt=""/></a>
                              </div>
-					</div>		
+					</div>
 					<div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
-						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
 							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
 						     <img src="images/t3.jpg" class="img-responsive" alt=""/></a>
-							
+
 					</div>
-					</div>				
+					</div>
 					<div class="portfolio icon mix_all" data-cat="icon" style="display: inline-block; opacity: 1;">
-						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
 							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
 						     <img src="images/t4.jpg" class="img-responsive" alt=""/></a>
-							 
+
 						</div>
-					</div>	
+					</div>
 					<div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
-						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
 							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
 						     <img src="images/t5.jpg" class="img-responsive" alt=""/></a>
-							 
+
 						</div>
-					</div>			
+					</div>
 					<div class="portfolio fun mix_all" data-cat="fun" style="display: inline-block; opacity: 1;">
-						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
 							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
 						     <img src="images/t6.jpg" class="img-responsive" alt=""/></a>
-							
+
 						</div>
 			      </div>
 				  <div class="portfolio fun mix_all" data-cat="fun" style="display: inline-block; opacity: 1;">
-						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
 							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
 							<img src="images/t7.jpg" class="img-responsive" alt=""/></a>
-							
+
 						</div>
 			      </div>
 				  <div class="portfolio icon mix_all" data-cat="fun" style="display: inline-block; opacity: 1;">
-						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
 							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
 						     <img src="images/t8.jpg" class="img-responsive" alt=""/></a>
-							 
+
 					   </div>
 			      </div>
-		   <div class="clearfix"></div>	
+		   <div class="clearfix"></div>
 	  </div>
 </div>
-</div>  
+</div>
 
 <!--/top-tours-->
-
 <div id="section-5" class="contact section">
 	  <div class="contact-head text-center">
 		  <h3>Contact Us</h3>
@@ -135,33 +132,29 @@
                         <br/>
                       </div>
 				  <div class="col-md-8 contact">
-                  
+
                   	<?php
-if(isset($_POST["sbmt"]))
-{
-	$cn=makeconnection();
-	$s="insert into contactus(Name,Phno,Email,Message) values('" . $_POST["t1"] ."','" . $_POST["t2"] ."','" . $_POST["t3"] ."','" . $_POST["t4"] ."')";
-	mysqli_query($cn,$s);
-	mysqli_close($cn);
-	echo "<script>alert('Record Save');</script>";
-}
-?>
-					  <form method="post">
-                      <table border="0" width="700px" height="500px">
-                      <tr><td align="left"> <input type="text" class="text" value=" Name"  name="t1" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your Name';}" required pattern="[a-zA-z1 _]{1,50}" title"Please Enter Only Characters and numbers between 1 to 50 for Name"></td></tr>
-                      <tr><td align="left"><input type="text" class="text" value=" Contact No" name="t2" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your Contact NO';}" required pattern="[0-9]{10,12}" title"Please Enter Only  numbers between 10 to 12 for Contact no"></td></tr>
-					 <tr><td align="left"> <input type="text" class="text" value="Email" name="t3" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your mail';}" required></td></tr>
-					 <tr><td><textarea onFocus="if(this.value == 'Message') this.value='';" name="t4" onBlur="if(this.value == '') this.value='Enter Message Here';" required/ >Message</textarea></td></tr>
-					  <tr><td><input type="submit" value="Send message" name="sbmt"></td></tr></table>
-					  <div class="clearfix"></div>
-					   </form>
-				   </div>
-				  <div class="clearfix"></div>
-			  </div>
-		  </div>
-	       
-
-
+                        if (isset($_POST["sbmt"])) {
+                            $cn=makeconnection();
+                            $s="insert into contactus(Name,Phno,Email,Message) values('" . $_POST["t1"] ."','" . $_POST["t2"] ."','" . $_POST["t3"] ."','" . $_POST["t4"] ."')";
+                            mysqli_query($cn, $s);
+                            mysqli_close($cn);
+                            echo "<script>alert('Record Save');</script>";
+                        }
+                        ?>
+                    <form method="post">
+                    <table border="0" width="700px" height="500px">
+                    <tr><td align="left"> <input type="text" class="text" value=" Name"  name="t1" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your Name';}" required pattern="[a-zA-z1 _]{1,50}" title"Please Enter Only Characters and numbers between 1 to 50 for Name"></td></tr>
+                    <tr><td align="left"><input type="text" class="text" value=" Contact No" name="t2" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your Contact NO';}" required pattern="[0-9]{10,12}" title"Please Enter Only  numbers between 10 to 12 for Contact no"></td></tr>
+					<tr><td align="left"> <input type="text" class="text" value="Email" name="t3" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your mail';}" required></td></tr>
+					<tr><td><textarea onFocus="if(this.value == 'Message') this.value='';" name="t4" onBlur="if(this.value == '') this.value='Enter Message Here';" required/ >Message</textarea></td></tr>
+					<tr><td><input type="submit" value="Send message" name="sbmt"></td></tr></table>
+            <div class="clearfix"></div>
+            </form>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
 
 <?php include('bottom.php'); ?>
 </body>
