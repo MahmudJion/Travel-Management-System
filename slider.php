809ffad2e597bd36da762c5c6facc7af0b0f654a
MahmudJion <mahmudh.jion02@gmail.com>
2022-10-14 11:54:55 +0100

    review and update

--- slider.php
+++ slider.php
@@ -7,22 +7,22 @@
 
 <body>
 <!---banner--->
-			<div id="section-1" class="section">
-			    <div id="top" class="callbacks_container">
-			      <ul class="rslides" id="slider4">
-			        <li>
-			          <img src="images/o-TRAVEL-facebook.jpg" alt="">
-					  <div class="caption">
-			     	  		<div class="header-info">
-							<h2><a href="#">Your Trusted Travel Packages and Services</a></h2>
-							<lable></lable>
-							<h1><a href="#">Go Travel & Tourism Ltd </a></h1>
-							</div>
-			          </div>
-			        
-			    </div>	         
-			    <div class="clearfix"> </div>
-				</div>
-<!---banner--->	
+    <div id="section-1" class="section">
+        <div id="top" class="callbacks_container">
+            <ul class="rslides" id="slider4">
+            <li>
+                <img src="images/o-TRAVEL-facebook.jpg" alt="">
+                <div class="caption">
+                    <div class="header-info">
+                    <h2><a href="#">Your Trusted Travel Packages and Services</a></h2>
+                    <lable></lable>
+                    <h1><a href="#">Go Travel & Tourism Ltd </a></h1>
+                    </div>
+                </div>
+
+        </div>
+    <div class="clearfix"> </div>
+</div>
+<!---banner--->
 </body>
 </html>
