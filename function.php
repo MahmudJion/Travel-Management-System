efc9095c3c3f3dd8e38f4c38f9f313f462277b85
MahmudJion <mahmudh.jion02@gmail.com>
2022-10-14 11:54:39 +0100

    review and update

--- function.php
+++ function.php
@@ -9,14 +9,13 @@
 <?php
 function makeconnection()
 {
-	$cn=mysqli_connect("localhost","root","","travel");
-	if(mysqli_connect_errno())
-	{
-		echo "failed to connect to mysqli:".mysqli_connect_error();
-	}
-	return $cn;
+    $cn=mysqli_connect("localhost", "root", "", "travel");
+    if (mysqli_connect_errno()) {
+        echo "failed to connect to mysqli:".mysqli_connect_error();
+    }
+    return $cn;
 }
-$cn=mysqli_connect("localhost","root","","travel");
+$cn=mysqli_connect("localhost", "root", "", "travel");
 ?>
 </body>
 </html>
